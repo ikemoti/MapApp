55fe66d121ed80e62c8673608d52744db81101e2
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-10-26 20:37:11 +0900

    複数ピン表示　ライブラリ導入まで

--- MapApp/DetailViewController.swift
+++ MapApp/DetailViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+//破棄したタイミングでピン選択終了
+//URLからSAFARI飛ぶ
+//
+public class DetailViewController: UIViewController {
+    let name: UILabel = .init()
+    public let text = String()
     
-
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(name)
+        name.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            name.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            name.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            name.heightAnchor.constraint(equalToConstant: 40),
+            name.widthAnchor.constraint(equalToConstant: 100)
+        ])
+        name.textColor = .black
+        name.text = "テスト"
+        
+    }
+    open func setData(text: String){
+        name.text = text
+    }
 }
--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -13,32 +13,17 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     let mapView: MKMapView = .init()
     var locationManager: CLLocationManager?
-    let testData: Sauna = .init(name: "フォーシーズンズホテル丸の内 東京", address:"    東京都 千代田区 丸の内１丁目１１−１ " , openTime: "13:20:00", closeTime: "13:20:00", timeException: nil, url: "https://sauna-ikitai.com/saunas/7385")
+    var pins: [MKPointAnnotation] = []
+    let testData: [Sauna] = [
+        .init(name: "フォーシーズンズホテル丸の内 東京", address:"    東京都 千代田区 丸の内１丁目１１−１ " , openTime: "13:20:00", closeTime: "13:20:00", timeException: nil, url: "https://sauna-ikitai.com/saunas/7385"),
+        .init(name: "ティップネス丸の内スタイル", address:" 東京都 千代田区 丸の内１丁目１−１ " , openTime: "07:00:00", closeTime: "23:00:00", timeException: nil, url: "https://sauna-ikitai.com/saunas/5559"),]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
-        locationManager = CLLocationManager()
-        locationManager?.delegate = self
-        locationManager?.requestWhenInUseAuthorization()
-        
-        let location:CLLocationCoordinate2D
-                   = CLLocationCoordinate2DMake(35.68154,139.752498)
-        mapView.setCenter(location,animated:true)
-        var region:MKCoordinateRegion = mapView.region
-        region.center = location
-        region.span.latitudeDelta = 0.02
-        region.span.longitudeDelta = 0.02
-//        let myPin = MKPointAnnotation()
-//        myPin.coordinate = location
-//        myPin.title = "テスト"
-//        myPin.subtitle = "さぶテスト"
-//        mapView.addAnnotation(myPin)
-        mapView.setRegion(region,animated:true)
-        mapView.delegate = self
-        let testPin = MKPointAnnotation ()
+        setMapAttributes()
         makePin(sauna: testData){ pin in
-            self.mapView.addAnnotation(pin)
+            self.mapView.addAnnotations(self.pins)
         }
     }
     
@@ -51,6 +36,20 @@ class ViewController: UIViewController, MKMapViewDelegate {
             mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
         ])
+    }
+    func setMapAttributes(){
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        let location:CLLocationCoordinate2D
+                   = CLLocationCoordinate2DMake(35.68154,139.752498)
+        mapView.setCenter(location,animated:true)
+        var region:MKCoordinateRegion = mapView.region
+        region.center = location
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        mapView.setRegion(region,animated:true)
+        mapView.delegate = self
     }
 
 
@@ -78,41 +77,38 @@ extension ViewController: CLLocationManagerDelegate {
     }
     //破棄されたタイミングでピンの選択解除
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        print(view.annotation?.title)
         let storyboard: UIStoryboard = self.storyboard!
         let nextView = storyboard.instantiateViewController(withIdentifier: "DetailViewController")
-        //ここがpushとは違う
         self.present(nextView, animated: true, completion: nil)
 
 
         
     }
     //中心に戻るメゾット
-    private func goBackCenter() {
-           self.mapView.setCenter(self.mapView.userLocation.coordinate, animated: false)
-           self.mapView.setUserTrackingMode(MKUserTrackingMode.followWithHeading, animated: false)
-       }
-    func makePin(sauna:Sauna, comletion: @escaping (MKPointAnnotation)->()){
-        let pin = MKPointAnnotation()
-        var saunaAdress: String = .init()
-        saunaAdress = sauna.address
-        var saunaLocation:CLLocationCoordinate2D = .init()
-        CLGeocoder().geocodeAddressString(saunaAdress) { placemarks, error in
-            if let lat = placemarks?.first?.location?.coordinate.latitude {
-                print("緯度 : \(lat)")
-                saunaLocation.latitude = lat
+//    private func goBackCenter() {
+//           self.mapView.setCenter(self.mapView.userLocation.coordinate, animated: false)
+//           self.mapView.setUserTrackingMode(MKUserTrackingMode.followWithHeading, animated: false)
+//    }
+    func makePin(sauna:[Sauna], comletion: @escaping (MKPointAnnotation)->()){
+        sauna.forEach {
+            let pin: MKPointAnnotation = .init()
+            var saunaAdress: String = .init()
+            var saunaLocation:CLLocationCoordinate2D = .init()
+            saunaAdress = $0.address
+            pin.title = $0.name
+            CLGeocoder().geocodeAddressString(saunaAdress) { placemarks, error in
+                if let lat = placemarks?.first?.location?.coordinate.latitude {
+                    saunaLocation.latitude = lat
+                }
+                if let lng = placemarks?.first?.location?.coordinate.longitude {
+                    saunaLocation.longitude = lng
+                }
+                pin.coordinate = saunaLocation
+                self.pins.append(pin)
+
+                comletion(pin)
             }
-            if let lng = placemarks?.first?.location?.coordinate.longitude {
-                print("経度 : \(lng)")
-                saunaLocation.longitude = lng
-            }
-            pin.coordinate = saunaLocation
-            pin.title = sauna.name
-            comletion(pin)
         }
     }
     
 }
-
-
-
